e8ed44e6cbe2a74dd66c8c72e9926a9b3e468a31
dean <deanbrennan13@hotmail.com>
2020-12-03 17:04:26 -0800

    Commit1

---
+++ HelloPython.py
@@ -0,0 +1,7 @@
+import datetime
+print("Hello world!")
+now = datetime.datetime.now()
+print ("Current date and time is ")
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print("Hello Test")
+print("Hello Test")
